cd1bd809fa94e7d00c80cc32618087d6a1160add
sinanthayyil <161109916+sinanthayyil@users.noreply.github.com>
2024-04-24 12:31:00 +0530

    chat-bot

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Inputfield from './components/Inputfield';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    <Inputfield/> 
+  
+    </div>
+  );
+}
+
+export default App;
